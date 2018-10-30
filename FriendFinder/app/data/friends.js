eda0b5ffb42461bfb39f81d0f577be1785cca912
Kaitlyn McLaughlin <kaitlyn@Kaitlyns-MacBook-Air.local>
2018-10-29 22:10:37 -0400

    add table data

---
+++ FriendFinder/app/data/friends.js
@@ -0,0 +1,29 @@
+var friendsArray = [
+    {
+    Name: "Laura",
+    Photo: "http://i67.tinypic.com/efs8eu.png",
+    Scores: [5, 4, 5, 4, 5, 4, 5, 4, 5, 4]
+    },
+    {
+    Name: "Agent Cooper",
+    Photo: "http://i65.tinypic.com/1t30jl.jpg",
+    Scores: [1, 2, 1, 2, 1, 2, 1, 2, 1, 2]
+    },
+    {
+    Name: "Leeland",
+    Photo: "http://i63.tinypic.com/2ekho3a.jpg",
+    Scores: [2, 3, 2, 3, 2, 3, 2, 3, 2, 3]
+    },
+    {
+    Name: "FBI Deputy Director Gordon Cole",
+    Photo: "http://i67.tinypic.com/2j0cg7l.jpg",
+    Scores: [1, 4, 5, 1, 5, 4, 1, 4, 5, 1]
+    },
+    {
+    Name: "Audrey",
+    Photo: "http://i67.tinypic.com/2ewp2qt.jpg",
+    Scores: [1, 2, 3, 4, 5, 4, 3, 2, 1, 2]
+    }
+]
+
+module.exports = friendsArray; 
