4a9a4a93663a338c082e835b8acc19b248e5b32a
Kaitlyn McLaughlin <kaitlyn@Kaitlyns-MacBook-Air.local>
2018-11-01 12:47:08 -0400

    survey page

--- FriendFinder/server.js
+++ FriendFinder/server.js
@@ -7,8 +7,8 @@ var app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require("./routing/apiRoutes")(app);
-require("./routing/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
                                                         // TEST PORT IS LISTENING
   app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
